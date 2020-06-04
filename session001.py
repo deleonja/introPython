a53d1248b2b682ffbdadf77c9df931c01f3dd3ab
deleonja <deleongarrido.jose@gmail.com>
2020-06-04 00:11:16 -0600

    v02

--- session001.py
+++ session001.py
@@ -1,3 +1,17 @@
+# 04 de junio, 2020
+# Autor: José Alfredo de León 
+
+# En esta sesión se platicó sobre el capítulo 1 del libro 
+# y se realizó este ejemplo para ilustrar un programa 
+# sencillo utilizando buenas prácticas de programación.
+
+# Cosas importantes: 
+#
+# 1. Antes de escribir el código pensar en el algoritmo.
+# 2. Escribir el algoritmo comentado. 
+# 3. Finalmente escribir el código según los pasos del 
+# algoritmo. 
+
 # Importar sys para poder leer desde terminal los números
 import sys
 
@@ -15,3 +29,18 @@ suma = primer_numero + segundo_numero
 print("Suma1 = " + str(suma))
 print("Suma2 = %2.0f" % suma)
 print("Suma3 = %3.1f" % suma)
+
+
+# Ejemplo: poner en terminal 'python3 session001.py 10.2 + 15.7'
+#
+# En terminal aparecerá:
+# Suma1 = 25.9
+# Suma2 = 26
+# Suma3 = 25.9
+# 
+#
+
+# Recordatorio: 
+# Se accede a la shell interactiva de python colocando en la 
+# terminal 'python3'
+
