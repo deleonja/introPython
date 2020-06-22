504a599ef63940908532813a7cd3b5ae5c590486
deleonja <deleongarrido.jose@gmail.com>
2020-06-22 13:42:14 -0600

    Unas modificaciones.

--- kronecker_JA.py
+++ kronecker_JA.py
@@ -3,16 +3,18 @@ M2 = [[1,0],[0,1]]
 
 resultado = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
 
-p = 2
-q = 2
+m = len(M1)
+n = len(M1[0])
+p = len(M2)
+q = len(M2[0])
 
-for i in range(2):
-    for j in range(2):
-        for k in range(2):
-            for l in range(2):
+for i in range(m):
+    for j in range(n):
+        for k in range(p):
+            for l in range(q):
                 alpha = p*(i) + k
                 beta = q*(j) + l
-                print(alpha, beta)
+                #print(alpha, beta)
                 resultado[alpha][beta] = M1[i][j]*M2[k][l]
 
 print(resultado)
